a85e83d4653900c78ae89b5cfd87537b013a2cf5
Julius Caesar Naag <junaag@my.cspc.edu.ph>
2026-01-15 19:12:24 +0800

    quick save

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->group('RawMaterials', function (RouteCollection $routes) {
+    $routes->post('AddRawMaterial', 'RawMaterialsController::addRawMaterial');
+});
