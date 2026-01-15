90b26a6fe67f01ef00e7855e51d21918661982c3
Julius Caesar Naag <junaag@my.cspc.edu.ph>
2026-01-15 21:09:14 +0800

    fixing some conflicts

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,15 +5,14 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+// $routes->get('/', 'Home::index');
+$routes->get('/Login', 'AuthenticationController::loginPage');
+$routes->get('/Dashboard', 'DashboardController::dashboard');
 
 $routes->group('RawMaterials', function ($routes) {
-    // $routes->get('/', 'Home::index');
-    $routes->get('/Login', 'AuthenticationController::loginPage');
-    $routes->get('/Dashboard', 'DashboardController::dashboard');
-    $routes->get('/RawMaterial', 'RawMaterialsController::rawMaterial');
     $routes->post('AddRawMaterial', 'RawMaterialsController::addRawMaterial');
 });
+
 $routes->group('MaterialCategory', function ($routes) {
     $routes->get('TestView', 'MaterialCategoryController::testView');
     $routes->post('Add', 'MaterialCategoryController::addCategory');
